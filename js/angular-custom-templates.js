674a7b37f7c9fca76484236d619156b7fea36551
Ajay Ganapathy <ajay.ganapathy@me.com>
2013-11-17 12:28:19 -0500

    changed file description of angular-custom-templates
    changed the description of angular-custom-templates to more accurately
    reflect the purpose of this file, which is to house custom angular
    directives that can't be found in angular.js itself or in angular UI
    gists.

--- js/angular-custom-templates.js
+++ js/angular-custom-templates.js
@@ -1,4 +1,6 @@
 /*
-this file contains js that creates custom html tags. It extends angular.js
-this file does NOT contain style/appearance attributes js helpers. For those attributes, see waffle-house-boostrap-theme.js
+this file contains custom angular directives. 
+Angular directives are HTML <tag *attributes*> that instruct the browser to give angular control over the DOM element that the tag creates.
+
+The directives in this file are either compound, complex, novel. Compound directives combine multiple existing angular directives into a single directive. Complex directives are heavily modified angular directives that could, but don't have to compound existing directives. Novel directives are completely new directives, built from scratch. 
 */
