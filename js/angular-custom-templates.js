aa5b3516a7e20db06f6bb293a04a42ed07200a2f
Ajay Ganapathy <ajay.ganapathy@me.com>
2013-11-16 19:14:37 -0500

    a new code structure
    parceled out different functionalities to different files for
    maintainability and nterchangeability

---
+++ js/angular-custom-templates.js
@@ -0,0 +1,4 @@
+/*
+this file contains js that creates custom html tags. It extends angular.js
+this file does NOT contain style/appearance attributes js helpers. For those attributes, see waffle-house-boostrap-theme.js
+*/
---
+++ js/waffle-house-bootstrap-theme.js
@@ -0,0 +1,3 @@
+/*
+this file contains javascript helper functions that enable animations, color changes, and other neat effects for waffle-house-bootstrap-theme.css
+*/
