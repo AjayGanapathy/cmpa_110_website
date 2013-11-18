5134e259927dc46a05c879e2263f6a4a5b8e63e1
Ajay Ganapathy <ajay.ganapathy@me.com>
2013-11-18 14:15:05 -0500

    Consolidating scripts
    moving angular directives, controllers, and services into a single .js
    file to reduce likelihood of circular dependencies and to make sure
    that all directives, controllers, and services are edited in context of
    each other

---
+++ js/custom-angular-scripts.js
@@ -0,0 +1,14 @@
+/*
+This script defines custom angular directives, controllers, and services:
+	Angular directives manipulate the DOM. They are bound to the DOM with corresponding HTML attributes. 
+	Angular controllers manipulate data. They access data through angular services. 
+	Angular services store data.  
+Angular is an MVC framework. 
+	Directives = view code
+	Controllers = controller code
+	Services = model code
+This file is just the tip of the iceberg. The majority of angular directives, controllers, and services are pre-built.
+*/
+/*Directives go here*/
+/*Controllers go here*/
+/*Services go here*/
