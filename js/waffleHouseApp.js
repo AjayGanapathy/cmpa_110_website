6ceafc3ec7e51823f6db1487b8cf2aa25f1c7c99
Ajay Ganapathy <ajay.ganapathy@me.com>
2013-11-18 15:28:09 -0500

    more refactoring, implementation of angular bootstrap
    scripts have been refactored again. This is probably not the final
    refactoring.
    
    On the bright side, I successfully injected the ui.bootstrap dependancy
    into the waffleHouseApp module

--- js/waffleHouseApp.js
+++ js/waffleHouseApp.js
@@ -1,14 +1,13 @@
 /*
-This script defines custom angular directives, controllers, and services:
+This script defines custom angular directives and controllers
 	Angular directives manipulate the DOM. They are bound to the DOM with corresponding HTML attributes. 
 	Angular controllers manipulate data. They access data through angular services. 
-	Angular services store data.  
 Angular is an MVC framework. 
 	Directives = view code
 	Controllers = controller code
 	Services = model code
 This file is just the tip of the iceberg. The majority of angular directives, controllers, and services are pre-built.
 */
-/*Directives go here*/
-/*Controllers go here*/
-/*Services go here*/
+//declare modules here
+var waffleHouseAppModule = angular.module('waffleHouseApp',['ui.bootstrap']);
+//configure modules here
